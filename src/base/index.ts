67471deb7f131e0b4b2698edb812f14a6b92642d
ncpa0cpl <szymonb21@gmail.com>
2023-06-17 17:06:41 +0200

    fix: added a catch to the mainloop start

--- src/base/index.ts
+++ src/base/index.ts
@@ -231,9 +231,14 @@ try {
 
   Output.setDefaultPrintMethod(print);
 
-  Mainloop.start().then((exitCode) => {
-    system.exit(exitCode);
-  });
+  Mainloop.start()
+    .then((exitCode) => {
+      system.exit(exitCode);
+    })
+    .catch((e) => {
+      print(String(e));
+      system.exit(1);
+    });
 
   main();
 } catch (e) {
